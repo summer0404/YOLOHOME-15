1032929eb5cac006da6bdb543cc4a9956192cb91
summer0404 <157554348+summer0404@users.noreply.github.com>
2025-03-06 14:19:07 +0700

    fix welcome card

--- src/components/home/WelcomeCard.jsx
+++ src/components/home/WelcomeCard.jsx
@@ -16,10 +16,14 @@ export default function WelcomeCard({name, temp, weather}) {
                 <h1 className="text-[#C45A01] font-bold text-xl sm:text-3xl md:text-4xl lg:text-5xl">Hello {name}!</h1>
                 <p className="text-[#BD5600] text-xs sm:text-sm md:text-base lg:text-base mt-2 max-w-[60%]">Welcome Home! The air quality is good & fresh you can go out today.</p>
             </div>
-            <div className="flex items-center space-x-1 mt-8">
-                <Thermometer className="text-[#242424] mr-2" />
-                <span className="text-[#242424] text-2xl mr-2">{temp}</span>
-                <span className='text-[#242424]'>Out door temperature</span>
+            <div className="flex items-center space-x-1 mt-2">
+                <Thermometer className="text-[#242424] mr-2 w-4 sm:w-5 md:w-6 lg:w-7" />
+                <span className="text-[#242424] text-sm sm:text-lg md:text-xl lg:text-2xl mr-2">
+                    {temp}
+                </span>
+                <span className="text-[#242424] text-xs sm:text-sm md:text-base lg:text-lg">
+                    Out door temperature
+                </span>
             </div>
             <div className="flex items-center space-x-1 mt-2">
                 <Cloudy className="text-[#242424] mr-2" />
